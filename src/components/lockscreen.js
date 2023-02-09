83fead4d6e25aa2da33284625ce68f111e90f278
unknown <markvalles15@gmail.com>
2023-02-09 21:06:17 +0800

    Add react

---
+++ src/components/lockscreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../css/lockscreen.css';
+
+class lockscreen extends React.Component{
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <wrapper>
+                <div class="background"></div>
+                <div class="opacity">
+
+                </div>
+            </wrapper>
+        );
+    }
+
+}
+
+export default lockscreen;
